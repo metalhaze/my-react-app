4f8089185a67f1e2affef59f500992d21112cb96
Alex Hazel <alexhazel@Lisas-MacBook-Pro.local>
2018-11-30 17:10:41 -0500

    Finished course 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => (
   <Layout>
     <div className="hero">
       <div className="heroGroup">
-        <h1>Hello Everyone!</h1>
-        <p>Learn to design and code Reactjs applications</p>
+        <h1>Learn to Design and Code React Apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
         <Link to="/page-2/">Watch the video</Link>
       </div>
     </div>
